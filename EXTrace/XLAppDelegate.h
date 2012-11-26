ed52533dbd45d1f9f9ed6b091cfcb57f92e3ab90
Albert <xieliang43@126.com>
2012-11-26 20:30:04 +0800

    init

---
+++ EXTrace/XLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XLAppDelegate.h
+//  EXTrace
+//
+//  Created by xie liang on 11/26/12.
+//  Copyright (c) 2012 xieliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
